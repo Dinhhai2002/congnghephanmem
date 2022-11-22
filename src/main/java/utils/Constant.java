902177e9387a093faa3b527bde88f417c30d820e
gitdanh <thanhdanhnguyen46@gmail.com>
2022-11-22 17:17:26 +0700

    project cũ

--- src/main/java/utils/Constant.java
+++ src/main/java/utils/Constant.java
@@ -3,7 +3,7 @@ package utils;
 public class Constant {
 	public static final String dir = "E:\\upload";
 	
-	public static final  String accessKey = "M8brj9K6E22vXoDB";	
+	public static final String accessKey = "M8brj9K6E22vXoDB";	
 	public static final String serectkey = "nqQiVSgDMy809JoPF6OzP5OdBUB550Y4";
 	public static final String orderType = "momo_wallet";
 	public static final String requestType="captureWallet";
@@ -12,7 +12,5 @@ public class Constant {
 	public static final String ipnUrl="http://localhost:8080/Shopee/pay/result";
 	public static final String Url="https://test-payment.momo.vn/v2/gateway/api/create";
 	public static final String IDMOMO="MOMO5RGX20191128";
-	
-
 	
 }
